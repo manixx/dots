7489b4262eff0a120eec5a595c77e99feee424bb
Manfred Mjka <manfred.mjka@sclable.com>
2020-11-09 14:34:59 +0100

    cleanup

--- zsh/functions/k8s-data.zsh
+++
@@ -1,5 +0,0 @@
-k8s_data() { # print k8s context 
-	if command -v kubectl &> /dev/null; then	
-		echo " %F{8}| k8s%f %F{green}$(kubectl config current-context)%f"
-	fi
-}
--- zsh/functions/vcs-data.zsh
+++
@@ -1,7 +0,0 @@
-vcs_data() { # print branch name 
-  vcs_info
-
-  if [ -n "$vcs_info_msg_0_" ]; then
-    echo " %F{8}| git%f %B${vcs_info_msg_0_}%b"
-  fi
-}
