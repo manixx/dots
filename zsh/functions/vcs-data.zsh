89393e78e8e50dd332c11a696fb07e0081506ddf
Manfred Mjka <manixx90@gmail.com>
2021-06-17 20:44:57 +0200

    chore: icons change

--- zsh/functions/vcs-data.zsh
+++ zsh/functions/vcs-data.zsh
@@ -2,6 +2,6 @@ vcs_data() { # print branch name
   vcs_info
 
   if [ -n "$vcs_info_msg_0_" ]; then
-    echo " %F{8}| git%f %B${vcs_info_msg_0_}%b"
+    echo " %F{8}| %f %B${vcs_info_msg_0_}%b"
   fi
 }
