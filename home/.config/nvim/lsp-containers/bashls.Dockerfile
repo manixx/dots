c0dfadd99f61b89e60be6d16d9770b2ef366f008
Manfred Krieger <manixx90@gmail.com>
2025-04-13 23:19:45 +0200

    chore: housekeeping

--- home/.config/nvim/lsp-containers/bashls.Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:22-alpine
-
-#RUN apk add --no-cache \
-#	shellcheck \
-RUN npm i -g \
-	bash-language-server
-ENV BASH_IDE_LOG_LEVEL=debug
-CMD ["bash-language-server", "start"]
--- home/.config/nvim/plugin/lsp.lua
+++ home/.config/nvim/plugin/lsp.lua
@@ -4,23 +4,9 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local localServer = {
 	'ccls',
 	'gopls',
-}
-
-local dockerServer = {
 	'bashls',
 }
 
 for _, lsp in ipairs(localServer) do
 	lspconfig[lsp].setup{ capabilities = capabilities }
-end
-
-for _, lsp in ipairs(dockerServer) do
-	cmd = {
-		'docker', 'run', '-i',
-		'-v', vim.fn.getcwd() .. ':' .. vim.fn.getcwd(),
-		'-w', vim.fn.getcwd(),
-		'lsp-containers/' .. lsp,
-	}
-
-	lspconfig[lsp].setup{ cmd = cmd, capabilities = capabilities }
 end
